b58db871b12ed808c3ce7aa1db3acc59655e8eaf
youssefkafa <youssef.kafa19941@gmail.com>
2021-04-30 14:40:18 +0300

    Update users.js

--- routers/users.js
+++ routers/users.js
@@ -54,7 +54,6 @@ router.delete('/deleteAll',verifyToken,(req,res)=>{
             firstName:req.body.firstName,
             lastName:req.body.lastName,
             email:req.body.email,
-            password:bcrypt.hashSync(req.body.password, 10),
             mobileNumber:req.body.mobileNumber,
             gender:req.body.gender,
             birthDay:req.body.birthDay,
